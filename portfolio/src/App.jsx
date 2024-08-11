9320df3416a6d71005d1f0d4e2a478907429e1f5
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2024-08-11 20:27:49 +0530

    Contact section

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './sections/contact/Contact'
 import Home from './sections/home/Home'
 import Projects from './sections/projects/Projects'
 import Skills from './sections/skills/Skills'
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <Projects />
       <Skills />
+      <Contact />
     </>
   )
 }
