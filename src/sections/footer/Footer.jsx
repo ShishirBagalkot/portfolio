6a3462c485846e25a38011508dc6a7f67087a6df
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2025-03-23 19:59:44 +0530

    Dynamic year fetch

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,10 +1,12 @@
 import styles from "./FooterStyles.module.css";
 
 export const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <section id="footer" className={styles.container}>
       <p>
-        &copy; 2024 Shishir Bagalkot
+        &copy; {currentYear} Shishir Bagalkot
         <br />
         All content is created with love, caffeine, and possibly a dash of procrastination.
       </p>
